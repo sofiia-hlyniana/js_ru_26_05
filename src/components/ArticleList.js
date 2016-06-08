b6280e21a46cb580e31d68214d9b4d1ed17fb67d
SG <sg@se7ensky.com>
2016-06-08 21:31:54 +0300

    daypicker settings

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,16 +1,20 @@
-import React, { PropTypes, Component } from 'react'
-import { findDOMNode } from 'react-dom'
-import Article from './Article'
-import Chart from './Chart'
-import oneOpen from '../decorators/oneOpen'
-import Select from 'react-select'
+import React, {PropTypes, Component} from 'react';
+import {findDOMNode} from 'react-dom';
+import Article from './Article';
+import Chart from './Chart';
+import oneOpen from '../decorators/oneOpen';
+import Select from 'react-select';
+import DayPicker, {DateUtils} from 'react-day-picker';
+import 'react-day-picker/lib/style.css';
 
 import 'react-select/dist/react-select.css'
 
 class ArticleList extends Component {
-
+  
   state = {
-    selected: null
+    selected: null,
+    from: null,
+    to: null
   };
 
   componentDidMount() {
@@ -19,12 +23,13 @@ class ArticleList extends Component {
   }
 
   render() {
-    const { articles, isOpen, openItem } = this.props;
+    const {articles, isOpen, openItem} = this.props;
+    const {from, to} = this.state;
 
     const articleItems = articles.map((article) => <li key={article.id}>
-      <Article article = {article}
-               isOpen = {isOpen(article.id)}
-               openArticle = {openItem(article.id)}
+      <Article article={article}
+               isOpen={isOpen(article.id)}
+               openArticle={openItem(article.id)}
       />
     </li>);
 
@@ -38,12 +43,19 @@ class ArticleList extends Component {
           <ul>
             {articleItems}
           </ul>
-          <Chart ref="chart" />
+          <Chart ref="chart"/>
+          <div className="datePickerResult">{this.getDateMessage()}</div>
+          <DayPicker
+              ref="daypicker"
+              numberOfMonths={2}
+              selectedDays={day => DateUtils.isDayInRange(day, {from, to})}
+              onDayClick={this.setDateRange.bind(this)}
+          />
           <Select
-              options = {options}
-              onChange = {this.handleChange}
-              value= {this.state.selected}
-              multi = {true}
+              options={options}
+              onChange={this.handleChange}
+              value={this.state.selected}
+              multi={true}
           />
         </div>
     )
@@ -53,12 +65,22 @@ class ArticleList extends Component {
     this.setState({
       selected
     })
+  };
+
+  setDateRange(e, day) {
+    const range = DateUtils.addDayToRange(day, this.state);
+    this.setState(range);
+  };
+
+  getDateMessage = () => {
+    const {from, to} = this.state;
+    if (from && to) return `you've selected dates between ${from.toLocaleDateString()} and ${to.toLocaleDateString()}`;
+    else return null;
   }
 }
 
 ArticleList.propTypes = {
   articles: PropTypes.array.isRequired,
-
   isOpen: PropTypes.func.isRequired,
   openItem: PropTypes.func.isRequired
 };
